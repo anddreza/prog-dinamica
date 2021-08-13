8683a3e9175874dc9237b7c5886722a15593435f
Andreza Silva <dedezasilva@live.com>
2021-08-13 11:26:05 -0400

    segunda aula foi de aritmética com números naturais

---
+++ mat1.jl
@@ -0,0 +1,24 @@
+# Aritmética com Números Naturais 
+
+# pedra, papel e tesoura 
+    # pedra = 0 
+    # papel = 1 
+    # tesoura = 2
+    # pedra < papel < tesoura < pedra 
+0%<3 1%3 2%3 3%3 
+jogador1 = "pedra"
+jogador2 = "papel"
+
+if jogador1 == jogador2 
+    empate 
+elseif jogador1 == (jogador2 + 1)%3 
+    jogador2 venceu 
+else 
+    jogador1 venceu 
+end 
+
+if jogador1 == "pedra" && jogador2 == "papel"
+    print("jogador2 venceu")
+elseif 
+    ...
+end 
